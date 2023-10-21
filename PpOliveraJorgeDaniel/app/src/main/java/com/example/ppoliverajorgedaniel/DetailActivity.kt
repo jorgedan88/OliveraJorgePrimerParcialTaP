08341503f812c72516d4c3502ea35198bec2b261
jorgedan88 <jorge.olivera@istea.com.ar>
2023-10-21 19:33:42 -0300

    Initial commit

---
+++ PpOliveraJorgeDaniel/app/src/main/java/com/example/ppoliverajorgedaniel/DetailActivity.kt
@@ -0,0 +1,30 @@
+package com.example.ppoliverajorgedaniel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+
+class DetailActivity : AppCompatActivity() {
+    private lateinit var tvnombreDetail: TextView
+    private lateinit var tvIngredientesDetail: TextView
+    private lateinit var ivLogo: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val receta = intent.getParcelableExtra<Receta>("receta")
+
+        tvnombreDetail = findViewById(R.id.tv_nombre_detail)
+        tvIngredientesDetail = findViewById(R.id.tv_ingredientesDetail)
+
+        ivLogo = findViewById(R.id.iv_logo_detail)
+
+
+        tvnombreDetail.text = receta?.nombre
+        tvIngredientesDetail.text = receta?.ingredientes
+
+        Glide.with(this).load(receta?.logo).into(ivLogo)
+    }
+}
